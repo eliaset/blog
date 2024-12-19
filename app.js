2d7ac2e1ec3185bedfd0fba85a1a3e14f436eaf6
Elias Shamil <eliasshamil@gmail.com>
2024-12-19 18:51:46 +0300

    add compose

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let editorContent = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Home Page",
     copyYear: new Date().getFullYear(),
+    post: editorContent,
   });
 });
 
@@ -25,6 +27,22 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact", {
     title: "Contact",
+    copyYear: new Date().getFullYear(),
+  });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose", {
+    title: "Contact",
+    copyYear: new Date().getFullYear(),
+  });
+});
+
+app.post("/submit", (req, res) => {
+  editorContent = req.body.content;
+  console.log(editorContent);
+  res.render("compose", {
+    post: editorContent,
     copyYear: new Date().getFullYear(),
   });
 });
